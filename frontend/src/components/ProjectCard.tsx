a8434fe3b9c31cb080a6c5aedededd906a5e8443
Emily Dong <emilyhfdong@gmail.com>
2022-06-25 12:32:48 -0400

    chore: remove unused vars

--- frontend/src/components/ProjectCard.tsx
+++ frontend/src/components/ProjectCard.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from "react"
+import React, { useRef, useState } from "react"
 import { Flex } from "rebass"
 import { useDimensions } from "../hooks"
 import { Project } from "../types"
